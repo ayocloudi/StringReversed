e51cdd129e17685a9c572ff9833c48b2c1386d97
byAyo <ayomide5462@gmail.com>
2023-10-23 23:22:35 +0100

    Add files via upload
    Program asks the user for a string input. It then reverses the string by iterating over it from the end to the start, and displays the reversed result.

---
+++ StringReversed.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class StringReversed {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a string to reverse: ");
+        String input = scanner.nextLine();
+
+        String reversed = reverseString(input);
+        System.out.println("Reversed String: " + reversed);
+    }
+
+    public static String reverseString(String str) {
+        String reversed = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed += str.charAt(i);
+        }
+        return reversed;
+    }
+}
